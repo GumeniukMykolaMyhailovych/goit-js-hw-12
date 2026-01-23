309e391523ec25104f72bfd9990110247736edb7
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-01-23 18:48:58 +0200

    Fix base paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/goit-js-hw-12/',
+  base: './',
 
   root: 'src',
 
