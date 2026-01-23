2c7544f15525b035589e1659bf5c7d094d067970
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-01-23 15:42:56 +0200

    HwJs-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 
-const API_KEY = 'PLACEHOLDER';
+const API_KEY = '54212177-5339591b3f1ced8f604dc8066';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function getImagesByQuery(query, page) {
+export async function getImagesByQuery(query, page = 1) {
   const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: page,
+    page,
     per_page: 15,
   };
+
+  console.log('ENV KEY =', API_KEY); // тимчасово для дебагу
 
   const response = await axios.get(BASE_URL, { params });
   return response.data;
